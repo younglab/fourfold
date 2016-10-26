da7cbd25025a50ffecde17572eced893af6005c7
Daniel Day <daniel.sindt.day@gmail.com>
2016-10-26 00:20:03 -0400

    Updated number of bootstrap iterations

--- bootstrap.r
+++ bootstrap.r
@@ -1,7 +1,7 @@
 library(compiler)
 enableJIT(3)
 
-NUM.OF.INTERATIONS <- 10
+NUM.OF.INTERATIONS <- 1000
 
 args <- commandArgs(T)
 
@@ -17,20 +17,9 @@ counts <- read.table(infile,sep='\t')
 N <- sum(counts$V3)
 R <- nrow(counts)
 
-#m <- matrix(0,nrow=R,ncol=NUM.OF.INTERATIONS)
 prob <- counts$V3/N
 
-#for( i in 1:NUM.OF.INTERATIONS) {
-#  idx <- sample.int(R,size=N,replace=T,prob=prob)
-  #n <- table(idx)
-#  n <- tabulate(idx,nbins=R)
-  
-#  m[,i] <- as.vector(n) 
-#  print(i)
-#}
-
 m <- do.call(cbind,lapply(1:NUM.OF.INTERATIONS,function(i) {
-  print(i)
   
   idx <- sample.int(R,size=N,replace=T,prob=prob)
   tabulate(idx,nbins=R)
