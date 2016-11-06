690a4336ea9e6a8d83944ebbefd19c63f8fefbb8
Daniel Day <daniel.sindt.day@gmail.com>
2016-11-06 15:43:18 -0500

    Updated interface for 4C profile smoothing

--- 4c-smooth-profiles.sh
+++ 4c-smooth-profiles.sh
@@ -2,10 +2,30 @@
 
 BASEDIR=$(dirname $0)
 ORGANISMDATABASE=$BASEDIR/organism-database.txt
+INPUTDIR="bootstrap"
 
-if [ $# -lt 5 ];
+TEMP=`getopt -o hi: -l input-dir: -n '4csmoothing' -- "$@"`
+eval set -- "$TEMP"
+
+while [ $# -ge 1 ]; do
+	case "$1" in
+	  --)
+	    shift
+	    break
+	    ;;
+	  -i|--input-dir)
+	    INPUTDIR="$2"
+	    shift
+	    ;;
+	  -h)
+	    ;;
+	esac
+	shift
+done
+
+if [ $# -lt 4 ];
 then
-  echo "4c-smooth-profiles.sh <XSLX table> <output dir> <bin size> <step size> <sample names>"
+  echo "4c-smooth-profiles.sh [options] <XSLX table> <output dir> <bin size> <step size> [sample names]"
   exit
 fi
 
@@ -18,10 +38,23 @@ shift
 STEPSIZE="$1"
 shift
 
+FILES="all"
+
+if [ $# -gt 0 ];
+then
+  FILES="$@"
+fi
+
 if [ ! -e "$SAMPLETABLE" ];
 then
   echo "Cannot find $SAMPLETABLE"
-  exit
+  exit 1
+fi
+
+if [ ! -d "$INPUTDIR" ];
+then
+  echo "Cannot find input directory $INPUTDIR"
+  exit 1
 fi
 
 if [ ! -e "$OUTPUTDIR" ];
@@ -35,7 +68,7 @@ then
   exit 1
 fi
 
-$BASEDIR/profile-smoothing.pl $SAMPLETABLE $BASEDIR $ORGANISMDATABASE $OUTPUTDIR $BINSIZE $STEPSIZE $@
+$BASEDIR/profile-smoothing.pl $SAMPLETABLE $BASEDIR $ORGANISMDATABASE $INPUTDIR $OUTPUTDIR $BINSIZE $STEPSIZE $@
 
 if [ $? -ne 0 ];
 then
