ce5a0f9b39de3ea548ef625a5b3a540b46739aee
Daniel Day <daniel.sindt.day@gmail.com>
2016-11-07 13:53:26 -0500

    Added helper script to write out where valid fragments were detected

---
+++ write-valid-fragments.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+
+my ($infile,$outfile) = @ARGV;
+
+my @frags;
+
+open(F,"<",$infile) or die "Cannot read: $!";
+open(O,">",$outfile) or die "Cannot write: $!";
+
+while(<F>) {
+  chomp;
+  
+  my ($chr,$s,$e,$l,$r) = split /\t/;
+  
+  unless($l eq "NA") {
+    my $beg = $s-$l;
+    print O "$chr\t$beg\t$s\n";
+  }
+  
+  unless($r eq "NA") {
+    my $end = $e+$r;
+    print O "$chr\t$e\t$end\n";
+  }
+}
+
+close(F);
+close(O);
