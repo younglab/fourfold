e67e3168bb55c2ee20df2c3dae5e6574ec257d5e
Daniel Day <daniel.sindt.day@gmail.com>
2016-11-30 23:04:51 -0500

    Updated usage of table validation script

---
+++ bin/4c-validate-table.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+
+function helpmenu() {
+  if [ $# -gt 0 ];
+  then
+    echo "$@"
+  fi
+  
+  echo "Syntax: 4c-validate-table.sh <sample XSLX table>"
+
+}
+
+if [ $# -lt 1 ];
+then
+  helpmenu "Error: not enough arguments"
+  exit 1
+fi
+
+SAMPLETABLE="$1"
+
+if [ ! -e "$SAMPLETABLE" ];
+then
+  helpmenu "Error: $SAMPLETABLE does not exist!"
+  exit 1
+fi
+
+$BASEDIR/4c-pipeline.sh --validate-table-only $SAMPLETABLE
