e895865087545b9e5b73483e1176db8b2148a4f8
Daniel Day <daniel.sindt.day@gmail.com>
2016-12-20 11:50:38 -0500

    Updated to try and smooth bootstrap profile in same way to make more accurate interval estimates

--- scripts/smoothing/smooth-single-profile.r
+++ scripts/smoothing/smooth-single-profile.r
@@ -5,7 +5,15 @@ args <- commandArgs(T)
 
 
 mean.proc <- function(idx,m) {
-  as.vector(mean(m[idx,4]))
+  
+  if(length(idx)>1) {
+    p <- m[idx,]
+    r <- as.vector(colMeans(m[,-c(1:3)]))
+  } else {
+    r <- m[idx,-c(1:3)]
+  }
+  
+  r
 }
 
 linear.proc <- function(idx,m) {
@@ -20,10 +28,7 @@ linear.proc <- function(idx,m) {
     pos <- p[1]
     l <- lm.fit(matrix(p[2:3],ncol=2),p[4]) ## Bx+a model
   }
-  
- # pos <- p[1,1]
-  #l <- lm.fit(p[,2:3],p[,4]) ## Bx+a model
-  
+
   cf <- coefficients(l)
   if(any(is.na(cf))) cf[is.na(cf)] <- 0
   
@@ -52,11 +57,11 @@ write("debug 0\n",file=stderr())
 chrom.sizes <- read.table(csize.file,sep='\t')
 
 signal <- read.table(measuredsignal.file,sep='\t')
-#bootstrap <- read.table(bootstrap.file,sep='\t')
+bootstrap <- read.table(bootstrap.file,sep='\t')
 
 write("debug 1\n",file=stderr())
 
-s <- GRanges(seqnames=as.character(signal[,1]),ranges=IRanges(signal[,2],width=1),strand='*',signal[,3])#,bootstrap[,-(1:2)])
+s <- GRanges(seqnames=as.character(signal[,1]),ranges=IRanges(signal[,2],width=1),strand='*',signal[,3],bootstrap[,-(1:2)])
 
 g <- unlist(GRangesList(lapply(1:nrow(chrom.sizes),function(i) {
   chr <- as.character(chrom.sizes[i,1])
@@ -76,17 +81,10 @@ m <- cbind(start(g)[queryHits(o)],1,start(s)[subjectHits(o)],as.matrix(mcols(s)[
 
 write("debug 3\n",file=stderr())
 
-
-#r <- lapply(split(m,queryHits(o)),function(ms) {
-#  if(is.vector(ms)) return(as.vector(c(ms[1])))#,quantile(ms[-1],probs=c(.025,.975)))))
-#  x <- colMeans(ms)
-  
-#  c(x[,1])#,as.vector(quantile(x,probs=c(.025,.975))))
-#})
-
 idx <- split(1:nrow(m),queryHits(o))
 
-r <- sapply(idx,proc,m=m)
+#r <- sapply(idx,proc,m=m)
+r <- do.call(rbind,lapply(idx,proc,m=m))
 
 write("debug 4",file=stderr())
 
