ba6b0792f37966dc5e16a1cf658e4b268992a9d2
Daniel Day <daniel.sindt.day@gmail.com>
2016-12-21 11:30:55 -0500

    Updated script to fix bugs in handling the data matrix, added some routines to clean up large chunks of memory as the script goes along as it uses a large amount of memory

--- scripts/smoothing/smooth-single-profile.r
+++ scripts/smoothing/smooth-single-profile.r
@@ -74,6 +74,10 @@ write("debug 1\n",file=stderr())
 s <- GRanges(seqnames=as.character(signal[,1]),ranges=IRanges(signal[,2],width=1),strand='*')#,signal[,3],bootstrap[,-(1:2)])
 d <- cbind(signal[,3],as.matrix(bootstrap[,-c(1:2)]))
 
+### clean up some memory as the next steps will take a lot
+rm(signal,bootstrap)
+gc()
+
 g <- unlist(GRangesList(lapply(1:nrow(chrom.sizes),function(i) {
   chr <- as.character(chrom.sizes[i,1])
   msize <- chrom.sizes[i,2]
@@ -88,23 +92,29 @@ write("debug 2\n",file=stderr())
 
 o <- findOverlaps(g,s)
 
-m <- cbind(start(g)[queryHits(o)],1,start(s)[subjectHits(o)],d[subjectHits(o),])
+#m <- cbind(start(g)[queryHits(o)],1,start(s)[subjectHits(o)],d[subjectHits(o),])
+m <- cbind(1,1,start(s),d)
 
 write("debug 3\n",file=stderr())
 
-idx <- split(1:nrow(m),queryHits(o))
+idx <- split((1:nrow(m))[subjectHits(o)],queryHits(o))
 
 #r <- sapply(idx,proc,m=m)
 #r <- do.call(rbind,lapply(idx,proc,m=m))
-r <- proc(idx,m)
+r <- proc(idx,m) 
 
 write("debug 4",file=stderr())
 
-print(r)
-save(r,file='tmp.Rdata')
+#print(r)
+#save(r,file='tmp.Rdata')
 
 
 gs <- g[as.integer(names(idx))]
+
+### clean up some memory as the next step can take a lot of memory
+rm(g,m,o,idx)
+gc() 
+
 df <- data.frame(seqnames(gs),start(gs),r)
 
 write.table(df,file=output.file,sep='\t',row.names=F,col.names=F,quote=F)
