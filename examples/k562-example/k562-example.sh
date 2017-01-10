c91535af79f44ce2fde8e62c5ad91242bff9dffe
Daniel Day <daniel.sindt.day@gmail.com>
2017-01-10 11:19:05 -0500

    Updated example script to make it more runnable out of the box, changed k562 super-enhancer file name to be more consistent with the others

--- examples/k562-example/k562-example.sh
+++ examples/k562-example/k562-example.sh
@@ -8,17 +8,17 @@ mkdir example
 cd example
 
 ### trims, maps 4C reads
-bsub -K -J 4c $BIN4C/4c-pipeline.sh k562-example-4c-data.xlsx &
+bsub -K -J 4c $BIN4C/4c-pipeline.sh ../k562-example-4c-data.xlsx &
 wait
 
 ### quantile normalizes profiles, saves to quantile-norm file, only looks at sample IDs beginning with K562
-$BIN4C/4c-normalize-samples.sh k562-example-4c-data.xlsx quantile quantile-norm K562.*
+$BIN4C/4c-normalize-samples.sh ../k562-example-4c-data.xlsx quantile quantile-norm K562.*
 
 ### smoothes quantile normalized profiles (remove --input=quantile-norm to smooth the unnormalized profiles)
-$BIN4C/4c-smooth-profiles.sh --input=quantile-norm k562-example-4c-data.xlsx smoothed-profiles 10000 50 K562.*
+$BIN4C/4c-smooth-profiles.sh --input=quantile-norm ../k562-example-4c-data.xlsx smoothed-profiles 10000 50 K562.*
 
 ### make individual replicate and per condition plots of K562 super-enhancers in MYC TAD
-$BIN4C/4c-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed k562-example-4c-data.xlsx chr8:130500000-130770818 sample-plots K562.* 
+$BIN4C/4c-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed ../k562-example-4c-data.xlsx chr8:130500000-130770818 sample-plots K562.* 
 
 ### makes joint plot between two K562 conditions
-$BIN4C/4c-multiple-sample-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed k562-example-4c-data.xlsx k562-example-profile-comparison.xlsx chr8:130500000-130770818 multiple-sample-plots
+$BIN4C/4c-multiple-sample-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed ../k562-example-4c-data.xlsx ../k562-example-profile-comparison.xlsx chr8:130500000-130770818 multiple-sample-plots
