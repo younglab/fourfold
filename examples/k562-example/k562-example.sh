57649366ec7f0aec77eacf63b8c7aa82699aeae9
Daniel Day <daniel.sindt.day@gmail.com>
2017-01-10 11:19:28 -0500

    Forgot to save some changes in the example script

--- examples/k562-example/k562-example.sh
+++ examples/k562-example/k562-example.sh
@@ -18,7 +18,7 @@ $BIN4C/4c-normalize-samples.sh ../k562-example-4c-data.xlsx quantile quantile-no
 $BIN4C/4c-smooth-profiles.sh --input=quantile-norm ../k562-example-4c-data.xlsx smoothed-profiles 10000 50 K562.*
 
 ### make individual replicate and per condition plots of K562 super-enhancers in MYC TAD
-$BIN4C/4c-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed ../k562-example-4c-data.xlsx chr8:130500000-130770818 sample-plots K562.* 
+$BIN4C/4c-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../k562-se.bed ../k562-example-4c-data.xlsx chr8:130500000-130770818 sample-plots K562.* 
 
 ### makes joint plot between two K562 conditions
-$BIN4C/4c-multiple-sample-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../K562-se.bed ../k562-example-4c-data.xlsx ../k562-example-profile-comparison.xlsx chr8:130500000-130770818 multiple-sample-plots
+$BIN4C/4c-multiple-sample-plots.sh --inputdir=smoothed-profiles --ylim-low=0 --ylim-high=800 --add-enhancers=../k562-se.bed ../k562-example-4c-data.xlsx ../k562-example-profile-comparison.xlsx chr8:130500000-130770818 multiple-sample-plots
