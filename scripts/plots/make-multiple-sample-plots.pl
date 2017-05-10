c496f985feb0c5229441c2418c78ca9fb06e8535
Daniel Day <daniel.sindt.day@gmail.com>
2017-05-10 10:46:47 -0400

    Fixed syntax error

--- scripts/plots/make-multiple-sample-plots.pl
+++ scripts/plots/make-multiple-sample-plots.pl
@@ -170,7 +170,7 @@ for( my $i = 0; $i < $nr; $i++ ) { ## row 1 (index 0) is the header line
 print "Making plots...\n";
 
 if( $multisampletable eq "all") {
-  runallsets($basedir,$genomecoord,$shading,$inputdir,$outputdir,$ylimlow,$ylimhigh,$enhancerfile,$promoterfile,$vertlines,$cialpha,$usecairopng\%samplegroups);
+  runallsets($basedir,$genomecoord,$shading,$inputdir,$outputdir,$ylimlow,$ylimhigh,$enhancerfile,$promoterfile,$vertlines,$cialpha,$usecairopng,\%samplegroups);
 } else {
   runfromtemplate($multisampletable,$basedir,$genomecoord,$shading,$inputdir,$outputdir,$ylimlow,$ylimhigh,$enhancerfile,$promoterfile,$vertlines,$cialpha,$usecairopng,\%samplegroups);
 } 
